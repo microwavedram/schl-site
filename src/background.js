4859db8ace0e86390c1e00773790a472543bb70c
microwavedram <89029844+microwavedram@users.noreply.github.com>
2024-04-29 13:45:33 +0000

    new background

--- src/background.js
+++ src/background.js
@@ -16,36 +16,48 @@ function drawFrame() {
     particles.forEach(particle => {
         ctx.beginPath()
         ctx.moveTo(w / 2 + particle.x, h / 2 + particle.y)
-        ctx.arc(w / 2 + particle.x, h / 2 + particle.y, 4, 0, Math.PI * 2)
+        ctx.arc(w / 2 + particle.x, h / 2 + particle.y, 2, 0, Math.PI * 2)
         ctx.fill()
         ctx.closePath()
 
         ctx.strokeStyle = "rgba(255,255,255,0.4)"
         ctx.strokeWidth = 1
 
+        particle.x += particle.xv
+        particle.y += particle.yv
+        
+        if (particle.x < -w/2) particle.xv = Math.abs(particle.xv)
+        if (particle.x > w/2) particle.xv = -Math.abs(particle.xv)
+        if (particle.y < -h/2) particle.yv = Math.abs(particle.yv)
+        if (particle.y > h/2) particle.yv = -Math.abs(particle.yv)
+
         particles.forEach(other => {
             const strength = Math.sqrt((particle.x - other.x) * (particle.x - other.x) + (particle.y - other.y) * (particle.y - other.y))
 
             if (strength < 100) {
+                ctx.strokeStyle = `rgba(255,255,255,${(100-strength)/100})`
                 ctx.beginPath()
                 ctx.moveTo(w / 2 + particle.x, h / 2 + particle.y)
                 ctx.lineTo(w / 2 + other.x, h / 2 + other.y)
                 ctx.stroke()
                 ctx.closePath()
             }
-
-            
         })
     })
 
     requestAnimationFrame(drawFrame)
 }
 
-for (let i = 0; i < 100; i++) {
+const TAU = Math.PI
+
+for (let i = 0; i < 200; i++) {
     particles.push({
-        x: (Math.random() - 0.5) * background.clientWidth,
-        y: (Math.random() - 0.5) * background.clientHeight,
+        x: Math.cos(i/TAU) * 100,
+        y: Math.sin(i/TAU) * 100,
+        xv: (Math.random() - 0.5) * 10,
+        yv: (Math.random() - 0.5) * 10
     })
 }
+
 
 drawFrame()
