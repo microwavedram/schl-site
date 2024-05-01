23bb8fa6f91385ce3e00049d08626a3fca68912b
microwavedram <89029844+microwavedram@users.noreply.github.com>
2024-05-01 20:06:22 +0000

    particle sim

--- src/background.js
+++ src/background.js
@@ -13,6 +13,8 @@ function drawFrame() {
 
     ctx.fillStyle = "rgba(255,255,255,1)"
 
+    
+    
     particles.forEach(particle => {
         ctx.beginPath()
         ctx.moveTo(w / 2 + particle.x, h / 2 + particle.y)
@@ -25,17 +27,29 @@ function drawFrame() {
 
         particle.x += particle.xv
         particle.y += particle.yv
-        
+
         if (particle.x < -w/2) particle.xv = Math.abs(particle.xv)
         if (particle.x > w/2) particle.xv = -Math.abs(particle.xv)
         if (particle.y < -h/2) particle.yv = Math.abs(particle.yv)
         if (particle.y > h/2) particle.yv = -Math.abs(particle.yv)
 
-        particles.forEach(other => {
-            const strength = Math.sqrt((particle.x - other.x) * (particle.x - other.x) + (particle.y - other.y) * (particle.y - other.y))
+        particle.xv *= 0.9
+        particle.yv *= 0.9
 
-            if (strength < 100) {
-                ctx.strokeStyle = `rgba(255,255,255,${(100-strength)/100})`
+        particles.forEach(other => {
+            const dx = particle.x - other.x
+            const dy = particle.y - other.y
+            
+            const strength = dx*dx + dy*dy
+
+
+            if (strength < 10000) {
+                particle.xv += dx * 0.0001
+                particle.yv += dy * 0.0001
+                other.xv += -dx * 0.0001
+                other.yv += -dy * 0.0001
+
+                ctx.strokeStyle = `rgba(255,255,255,${(10000-strength)/10000})`
                 ctx.beginPath()
                 ctx.moveTo(w / 2 + particle.x, h / 2 + particle.y)
                 ctx.lineTo(w / 2 + other.x, h / 2 + other.y)
@@ -50,12 +64,14 @@ function drawFrame() {
 
 const TAU = Math.PI
 
-for (let i = 0; i < 200; i++) {
+const count = 1000
+const C = TAU/count
+for (let i = 0; i < count; i++) {
     particles.push({
-        x: Math.cos(i/TAU) * 100,
-        y: Math.sin(i/TAU) * 100,
-        xv: (Math.random() - 0.5) * 10,
-        yv: (Math.random() - 0.5) * 10
+        x: Math.cos(i/C) * 500,
+        y: Math.sin(i/C) * 500,
+        xv: Math.cos(i/C) * -80,
+        yv: Math.sin(i/C) * -80,
     })
 }
 
